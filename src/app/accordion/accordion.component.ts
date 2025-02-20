67efadc593a3835e71971a999c72b2546e554fb2
Danilo Teixeira <danilo@lendwell.ie>
2025-02-20 12:28:38 -0300

    Push project

---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,89 @@
+import {
+  CDK_ACCORDION,
+  CdkAccordion,
+  CdkAccordionItem,
+  CdkAccordionModule,
+} from "@angular/cdk/accordion";
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  signal,
+  viewChild,
+} from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: "app-accordion",
+  imports: [CdkAccordionModule],
+  providers: [{ provide: CDK_ACCORDION, useExisting: AccordionComponent }],
+
+  template: ` <cdk-accordion class="example-accordion">
+    <ng-content />
+  </cdk-accordion>`,
+})
+export class AccordionComponent extends CdkAccordion {
+  constructor() {
+    super();
+  }
+}
+
+@Component({
+  selector: "app-accordion-item",
+  imports: [CdkAccordionItem],
+  template: `
+    <cdk-accordion-item
+      #accordionItem="cdkAccordionItem"
+      class="example-accordion-item"
+      tabindex="0"
+      [attr.id]="'accordion-header-' + index()"
+      [attr.aria-expanded]="accordionItem.expanded"
+      [attr.aria-controls]="'accordion-body-' + index()"
+    >
+      <div
+        class="example-accordion-item-header"
+        role="button"
+        (click)="accordionItem.toggle()"
+      >
+        <ng-content select="[accordion-header]" />
+        <span class="example-accordion-item-description">
+          Click to {{ accordionItem.expanded ? "close" : "open" }}
+        </span>
+      </div>
+
+      <div
+        class="example-accordion-item-body"
+        role="region"
+        [style.display]="accordionItem.expanded ? '' : 'none'"
+        [attr.id]="'accordion-body-' + index()"
+        [attr.aria-labelledby]="'accordion-header-' + index()"
+      >
+        <ng-content />
+      </div>
+    </cdk-accordion-item>
+  `,
+})
+export class AccordionItemComponent implements AfterViewInit {
+  readonly #destroyRef = inject(DestroyRef);
+
+  readonly accordionItem =
+    viewChild.required<CdkAccordionItem>("accordionItem");
+
+  readonly index = input<number>();
+
+  readonly #expanded = signal(false);
+
+  ngAfterViewInit() {
+    this.accordionItem()
+      .expandedChange.pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe((expanded) => {
+        this.#expanded.set(expanded);
+      });
+  }
+
+  open = () => this.accordionItem().open();
+  close = () => this.accordionItem().close();
+  toggle = () => this.accordionItem().toggle();
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import {
+  AccordionComponent,
+  AccordionItemComponent,
+} from "./accordion/accordion.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+  selector: "app-root",
+  imports: [AccordionItemComponent, AccordionComponent],
 
-    <router-outlet />
+  template: `
+    <app-accordion multi>
+      @for (item of items; track $index) {
+      <app-accordion-item [index]="$index" #accordionItem>
+        <span accordion-header>{{ item }}</span>
+
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+          excepturi incidunt ipsum deleniti labore, tempore non nam doloribus
+          blanditiis veritatis illo autem iure aliquid ullam rem tenetur
+          deserunt velit culpa?
+        </div>
+      </app-accordion-item>
+
+      }
+    </app-accordion>
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        display: flex;
+        gap: 1rem;
+
+        & > * {
+          flex: 1;
+        }
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'cdk-accordion';
+  readonly items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 }
